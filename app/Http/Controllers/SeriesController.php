365be22fa0c8833dd5ae9ea84fa621c0e23beb28
Wallace Vidal <wallacevff@hotmail.com>
2021-09-17 16:09:24 -0300

    First Commit

---
+++ app/Http/Controllers/SeriesController.php
@@ -0,0 +1,29 @@
+<?php 
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+
+class SeriesController extends Controller
+{
+    public function listarSeries(Request $request){
+        //echo $request->url();
+        //var_dump($request->query());
+        exit();
+        $series = [
+            'Grey\'s Anatomy',
+            'Lost',
+            'Agents of SHIELD'
+        ];
+    
+        $html = "<ul>";
+        $html .= "</ul>";
+        foreach ($series as $serie){
+            $html .= "<li>" . $serie . "</li>";
+            
+        }
+        $html .= "</ul>";
+        echo $html;
+    }
+}
+
+?>
