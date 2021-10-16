1f7e4356aa954a191f8da5827667d1284af9224e
Wallace Vidal <wallacevff@hotmail.com>
2021-10-16 18:00:41 -0300

    gitignore deleted

--- resources/views/Series/index.blade.php
+++ resources/views/Series/index.blade.php
@@ -23,21 +23,25 @@ Séries
     </thead>
     <tbody>
 
-        <?php foreach($series as $serie): ?>
-        <tr>
-            <td>{{$serie->nome}}</td>
-            <td><a href='/series/editar/{{$serie->id}}' class='btn btn-secondary'><i class="fas fa-edit"></i></td>
-            <td><a href='/series/{{$serie->id}}/temporadas' class='btn btn-info'><i class="fas fa-external-link-alt"></i></a>
-            <td>
-                <form method="post" action="/series/{{$serie->id}}"
-                onsubmit="return confirm('Tem certeza que deseja remover {{addslashes($serie->nome)}}?')">
-                @csrf
-                @method('DELETE')
-                    <button class='btn btn-danger'><i class="fas fa-trash-alt"></i></button>
-                </form>
-            </td>
-            <!-- <td><button class='btn btn-brown' action="excluir" method="get">Excluir</button></td> -->
-        </tr>
+        <?php foreach ($series as $serie) : ?>
+            <tr>
+                <td>
+                    <span id="nome-serie-{{ $serie->id }}">{{$serie->nome}}</span>
+                    <div class="input-group w-50" hidden id="input-nome-serie-{{ $serie->id }}">
+                        <input type="text" class="form-control"
+                    </div>
+                </td>
+                <td><a href='/series/editar/{{$serie->id}}' class='btn btn-secondary'><i class="fas fa-edit"></i></td>
+                <td><a href='/series/{{$serie->id}}/temporadas' class='btn btn-info'><i class="fas fa-external-link-alt"></i></a>
+                <td>
+                    <form method="post" action="/series/{{$serie->id}}" onsubmit="return confirm('Tem certeza que deseja remover {{addslashes($serie->nome)}}?')">
+                        @csrf
+                        @method('DELETE')
+                        <button class='btn btn-danger'><i class="fas fa-trash-alt"></i></button>
+                    </form>
+                </td>
+                <!-- <td><button class='btn btn-brown' action="excluir" method="get">Excluir</button></td> -->
+            </tr>
         <?php endforeach; ?>
 
     <tbody>
