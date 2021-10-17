d426e39dd010663f5b48b3e222099db9ec663e87
Wallace Vidal de Figueiredo Fortuna <wallacevff@hotmail.com>
2021-10-17 20:42:15 -0300

    Nro. de episódios por temporada.

--- resources/views/Temporadas/index.blade.php
+++ resources/views/Temporadas/index.blade.php
@@ -5,8 +5,14 @@ Temporadas de {{$serie->nome}}
 @endsection
 
 @section('conteudo')
-    <ul class='list-group'>
-        @foreach($temporadas as $temporada)
-            <li class='list-group-item'>Temporada {{$temporada->numero}}</li>
-        @endforeach
-@endsection
+<ul class='list-group'>
+    @foreach($temporadas as $temporada)
+    <li class='list-group-item d-flex f justify-content-between align-items-center'>
+        <a href="#">Temporada {{$temporada->numero}}
+        </a>
+        <span class="badge badge-secondary">
+           0 / {{ $temporada->episodios->count() }}
+        </span>
+    </li>
+    @endforeach
+    @endsection
